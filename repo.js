2624159644b3c83cd2568be584a3df49cb2e87a0
PookaM <pooka251@hotmail.com>
2017-09-25 22:15:40 +0300

    Fix description

--- repo.js
+++ repo.js
@@ -2,7 +2,7 @@
 	"contact": "pooka251@gmail.com",
 	"id": "PookChang'e",
 	"patches": {
-		"reimu_rod": "Restore Reimu's Rod"
+		"reimu_rod": "Restore Reimu's purification rod in TH15 and TH16"
 	},
 	"servers": [
 		"http://thcrap.nmlgc.net/repos/PookChang'e/",
