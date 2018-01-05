fd70fee267aa7c994a9d81abe1c92f17e13b51ad
PookaM <32255486+PookaM@users.noreply.github.com>
2018-01-05 21:18:16 +0200

    Alphabetically sorting patches

--- repo.js
+++ repo.js
@@ -2,8 +2,8 @@
 	"contact": "pooka251@gmail.com",
 	"id": "PookChang'e",
 	"patches": {
-		"reimu_rod": "Restore Reimu's purification rod in TH15 and TH16"
 		"kirisa_ha": "Help Marisa Kirisa and Reimu Ha transcend into the games"
+		"reimu_rod": "Restore Reimu's purification rod in TH15 and TH16"
 	},
 	"servers": [
 		"http://thcrap.nmlgc.net/repos/PookChang'e/",
