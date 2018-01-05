d7f367c40daeba7a60a56ec63c15e2dfc69c882e
PookaM <32255486+PookaM@users.noreply.github.com>
2018-01-05 21:07:37 +0200

    Adding patch.js

---
+++ kirisa_ha/patch.js
@@ -0,0 +1,12 @@
+{
+    "dependencies": [
+        "nmlgc/base_tsa",
+        "thpatch/lang_en/"
+    ],
+    "id": "kirisa_ha",
+    "servers": [
+        "http://thcrap.nmlgc.net/repos/PookChang'e/kirisa_ha/",
+        "http://mirrors.thpatch.net/PookChang'e/kirisa_ha/"
+    ],
+    "title": "Help Marisa Kirisa and Reimu Ha transcend into the games"
+}
