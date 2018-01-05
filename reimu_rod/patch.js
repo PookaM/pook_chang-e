9b5f9a5065cd67224e64ad0d6d935a6c535eb3f0
PookaM <32255486+PookaM@users.noreply.github.com>
2018-01-05 13:33:54 +0200

    Including dependencies

--- reimu_rod/patch.js
+++ reimu_rod/patch.js
@@ -1,8 +1,11 @@
 {
-	"id": "reimu_rod",
-	"servers": [
-		"http://thcrap.nmlgc.net/repos/PookChang'e/reimu_rod/",
-		"http://mirrors.thpatch.net/PookChang'e/reimu_rod/"
-	],
-	"title": "Restore Reimu's purification rod in TH15 and TH16"
+    "dependencies": [
+        "nmlgc/base_tsa"
+    ],
+    "id": "reimu_rod",
+    "servers": [
+        "http://thcrap.nmlgc.net/repos/PookChang'e/reimu_rod/",
+        "http://mirrors.thpatch.net/PookChang'e/reimu_rod/"
+    ],
+    "title": "Restore Reimu's purification rod in TH15 and TH16"
 }
