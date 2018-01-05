1c045fc79a2143db5559766a511ff745b38a0cba
PookaM <32255486+PookaM@users.noreply.github.com>
2018-01-05 21:19:43 +0200

    And added missing comma

--- repo.js
+++ repo.js
@@ -2,7 +2,7 @@
 	"contact": "pooka251@gmail.com",
 	"id": "PookChang'e",
 	"patches": {
-		"kirisa_ha": "Help Marisa Kirisa and Reimu Ha transcend into the games"
+		"kirisa_ha": "Help Marisa Kirisa and Reimu Ha transcend into the games",
 		"reimu_rod": "Restore Reimu's purification rod in TH15 and TH16"
 	},
 	"servers": [
